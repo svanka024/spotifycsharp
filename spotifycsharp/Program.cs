a64e426ae30dd6e4d2e2c308e0386f01f228d91a
svanka024 <1196688@student.roc-nijmegen.nl>
2023-05-15 13:06:58 +0200

    Add project files.

---
+++ spotifycsharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace spotifycsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
