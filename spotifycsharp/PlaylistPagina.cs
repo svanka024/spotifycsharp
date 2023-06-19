88a28a76d28a8053124ba3c39df8ea35da346953
jaspervanuden <jaspervanuden03@gmail.com>
2023-06-19 23:36:45 +0200

    zo goed als af,

--- spotifycsharp/PlaylistPagina.cs
+++ spotifycsharp/PlaylistPagina.cs
@@ -8,7 +8,6 @@ namespace spotifycsharp
 {
     internal class PlaylistPagina
     {
-
         private static List<Playlist> Playlists = new List<Playlist>();
 
         public void DisplayPlaylistPagina()
@@ -26,7 +25,7 @@ namespace spotifycsharp
 
                 switch (option)
                 {
-                    case 1: 
+                    case 1:
                         MaakNieuwePlaylist();
                         break;
 
@@ -36,9 +35,140 @@ namespace spotifycsharp
 
                     case 3:
                         Console.WriteLine("Terug naar het menu...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Geen geldige keuze");
 
                 }
+                Console.WriteLine();
+            } while (option != 3);
+        }
+        private static void MaakNieuwePlaylist()
+        {
+            Console.WriteLine("Naam van je nieuwe playlist:");
+            string playlistNaam = Console.ReadLine();
+
+            Playlist nieuwPlaylist = new Playlist(playlistNaam);
+            Playlists.Add(nieuwPlaylist);
+
+            Console.WriteLine($"Nieuwe playlist '{playlistNaam}' is gemaakt");
+            Console.WriteLine();
+
+            Console.WriteLine($"{nieuwPlaylist.Naam}");
+            Console.WriteLine();
+
+            Console.WriteLine($"{nieuwPlaylist.Liedje.Count + 1}. Ga terug naar Playlist pagina");
+            Console.Write("liedje: ");
+            int option = int.Parse(Console.ReadLine());
+
+            if (option == nieuwPlaylist.Liedje.Count + 1)
+            {
+                Console.WriteLine();
+                return;
             }
+
+            if (option < 1 || option > nieuwPlaylist.Liedje.Count)
+            {
+                Console.WriteLine("Geen geldige optie.");
+                Console.WriteLine();
+                return;
+            }
+        }
+
+        private static void VerwijderPlaylist(List<Playlist> playlists)
+        {
+            Console.WriteLine("Welke playlist wil je verwijderen?");
+            Console.WriteLine("Playlist: ");
+            int optie = int.Parse(Console.ReadLine());
+
+            if (optie < 1 || optie > playlists.Count)
+            {
+                Console.WriteLine("Geen geldige optie.");
+                Console.WriteLine();
+                return;
+            }
+            Playlist playlistVerwijder = playlists[optie - 1];
+            playlists.Remove(playlistVerwijder);
+
+            Console.WriteLine($"Playlist '{playlistVerwijder.Naam}' is verwijderd.");
+        }
+
+        private static void BekijkPlaylist()
+        {
+            Console.WriteLine("Playlist Overzicht:");
+            Console.WriteLine();
+
+            if (playlists.Count == 0)
+            {
+                Console.WriteLine("Er zijn geen playlist beschikbaar.");
+                Console.WriteLine();
+                return;
+            }
+            for (int i = 0; i < playlists.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {playlists[i].Naam}");
+            }
+
+            Console.WriteLine($"{playlists.Count + 1}. Verwijder een playlist");
+            Console.WriteLine($"{playlists.Count + 2}. Ga terug naar het menu");
+            Console.Write("Playlist: ");
+            int optie = int.Parse(Console.ReadLine());
+
+            if (optie == playlists.Count + 2)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            if (optie < 1 || optie > playlists.Count + 1)
+            {
+                Console.WriteLine("Geen geldige optie.");
+                Console.WriteLine();
+                return;
+            }
+
+            if (optie == playlists.Count + 1)
+            {
+                VerwijderPlaylist(playlists);
+                Console.WriteLine();
+                return;
+            }
+
+            Playlist geselecteerdePlaylist = playlists[optie - 1];
+
+            Console.WriteLine();
+            Console.WriteLine($"{geselecteerdePlaylist.Naam}:");
+            Console.WriteLine();
+
+            for (int i = 0; i < geselecteerdePlaylist.Liedje.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {geselecteerdePlaylist.Songs[i].ToString()}");
+            }
+
+            Console.WriteLine($"{geselecteerdePlaylist.Liedje.Count + 1}. Ga terug naar het menu");
+            Console.Write("Liedje: ");
+            optie = int.Parse(Console.ReadLine());
+
+            if (optie == geselecteerdePlaylist.Liedje.Count + 1)
+            {
+                Console.WriteLine();
+                return;
+            }
+
+            if (optie < 1 || optie > geselecteerdePlaylist.Liedje.Count)
+            {
+                Console.WriteLine("Geen geldige optie");
+                Console.WriteLine();
+                return;
+            }
+
+            Liedje geselecteerdeLiedje = geselecteerdePlaylist.Liedje[optie - 1];
+
+            Console.WriteLine();
+            Console.WriteLine($"Nu speeld '{geselecteerdeLiedje.ToString()}' van '{geselecteerdePlaylist.Liedje}'!");
+            Console.WriteLine();
+
         }
     }
 }
